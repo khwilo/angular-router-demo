342530649342280f5a157657565c23544f538d95
Khwilo Kabaka <khwilowatai@gmail.com>
2019-09-02 12:40:17 +0300

    Remove references to html and css template files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <h2><a [routerLink]="'/contacts'">Contacts</a></h2>
     <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+  `
 })
 export class AppComponent {
   title = 'angular-router-demo';
