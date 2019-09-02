c7a9b9f147d5324e0ebb81a3048839a4496e47fa
Khwilo Kabaka <khwilowatai@gmail.com>
2019-09-02 10:12:52 +0300

    Add the /contacts navigation link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    <h2><a [routerLink]="'/contacts'">Contacts</a></h2>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
