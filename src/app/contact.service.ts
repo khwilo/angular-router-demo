9ee86880cbfc6423a3eabf9502af102edfba31c5
Khwilo Kabaka <khwilowatai@gmail.com>
2019-09-02 09:51:14 +0300

    Add API request methods to be intercepted and sent to the in-memory backend

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  API_URL = '/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getContacts() {
+    return this.http.get(this.API_URL + 'contacts');
+  }
+
+  getContact(contactId) {
+    return this.http.get(`${this.API_URL + 'contacts'}/${contactId}`);
+  }
 }
