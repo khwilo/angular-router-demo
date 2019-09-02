68927f40628ba7675bd36223d19e45e0cbeae2d9
Khwilo Kabaka <khwilowatai@gmail.com>
2019-09-02 10:02:31 +0300

    Import the routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { BackendService } from './backend.service';
+
+import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
@@ -15,6 +17,7 @@ import { ContactDetailComponent } from './contact-detail/contact-detail.componen
     ContactDetailComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     InMemoryWebApiModule.forRoot(BackendService)
   ],
